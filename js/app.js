99f227f5e44ce906241977da3f0c77817145d6a1
Daniel Williams <dfwii00@gmail.com>
2022-08-24 12:25:54 -0700

    Setting up for audio playback
    css-tricks has audio asset playback code. Our old code was for a default audio device in browser.
    
    This direction allows arbitrary loading and playback of audio.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-myAudioElement = new Audio('./assets/click.mp3');
-
-myAudioElement.addEventListener("clickTone", (event) => {
-    /* the audio is now playable; play it if permissions allow */
-    myAudioElement.play();
-  });
-
 function Toggle(tempo) {
     //alert(tempo);
     myAudioElement.dispatchEvent(myAudioElement);
 }
+
+// The following lines are from https://css-tricks.com/form-validation-web-audio/
+const context = new window.AudioContext();
+
+function playFile(filepath) {
+  // see https://jakearchibald.com/2016/sounds-fun/
+  // Fetch the file
+  fetch(filepath)
+    // Read it into memory as an arrayBuffer
+    .then(response => response.arrayBuffer())
+    // Turn it from mp3/aac/whatever into raw audio data
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      // Now we're ready to play!
+      const soundSource = context.createBufferSource();
+      soundSource.buffer = audioBuffer;
+      soundSource.connect(context.destination);
+      soundSource.start();
+    });
+}
+
+let successButton = document.querySelector("#success");
+successButton.addEventListener("click", function() {
+  playFile('./assets/click.mp3');
+});
+
+/*
+let errorButton = document.querySelector("#error");
+errorButton.addEventListener("click", function() {
+  playFile('https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/error.mp3');
+});
+*/
