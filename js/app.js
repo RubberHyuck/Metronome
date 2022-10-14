bfb4307214f088636805b6b48c47bf24b2730f82
Daniel Williams <dfwii00@gmail.com>
2022-10-14 15:30:01 -0700

    foo

--- js/app.js
+++ js/app.js
@@ -1,41 +1,62 @@
-function Toggle(tempo) {
-    //alert(tempo);
-    myAudioElement.dispatchEvent(myAudioElement);
-}
-
 // The following lines are from https://css-tricks.com/form-validation-web-audio/
 const context = new window.AudioContext();
+let clickTone = context.createBufferSource();
+let clickBuffer = {};
+let clockSpring;
 
-window.onload = (event) => {  
-  function playFile(filepath) {
-    // see https://jakearchibald.com/2016/sounds-fun/
-    // Fetch the file
-    fetch(filepath)
-      // Read it into memory as an arrayBuffer
-      .then(response => response.arrayBuffer())
-      // Turn it from mp3/aac/whatever into raw audio data
-      .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
-      .then(audioBuffer => {
-        // Create a source:
-        // This represents a playback head.
-        const soundSource = context.createBufferSource();
-        // Give it the audio data we loaded:
-        soundSource.buffer = audioBuffer;
-        // Plug it into the output:
-        soundSource.connect(context.destination);
-        // Now we're ready to play!
-        soundSource.start();
-      });
-  }
+window.onload = (event) => {
   
-  let successButton = document.querySelector("#success");
-  
-  successButton.addEventListener("click", function() {
-    playFile('./assets/click.mp3');
+  loadFile('./assets/click.mp3');
+
+  let toggleButton = document.querySelector("#toggle");  
+  toggleButton.addEventListener("click", function() {
+    let tempoSlider = document.querySelector("#tempo"); 
+    toggle(tempoSlider.value);
   });
-  
-  let errorButton = document.querySelector("#error");
-  errorButton.addEventListener("click", function() {
-    playFile('https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/error.mp3');
-  });
+
 };
+  
+function loadFile(filepath) {
+  // see https://jakearchibald.com/2016/sounds-fun/
+  // Fetch the file
+  
+  fetch(filepath)
+  // Read it into memory as an arrayBuffer
+  .then(response => response.arrayBuffer())
+  // Turn it from mp3/aac/whatever into raw audio data
+  .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+  .then(audioBuffer => {
+    // Create a source:
+    // This represents a playback head.
+    const soundSource = context.createBufferSource();
+    // Give it the audio data we loaded:
+    clickBuffer = audioBuffer;
+  });
+}
+
+function playSound() {
+  let localTone = context.createBufferSource();
+  localTone.buffer = clickBuffer;
+  // Plug it into the output:
+  localTone.connect(context.destination);
+  // Now we're ready to play!
+  localTone.start();
+}
+
+function tempoClock(tempo = 90) {
+  console.log(tempo);
+  let delay = 60000/tempo;
+  if (!clockSpring) {
+      let clockSpring = setInterval(function() {
+      playSound();
+    }, delay);
+  }
+}
+
+function toggle(tempo = 90) {
+  if (!clockSpring) {
+    tempoClock(tempo);
+  } else {
+    clearInterval(clockSpring);
+  }
+}
